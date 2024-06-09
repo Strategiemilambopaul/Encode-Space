092d043b5bbfdfe4ddb67f5e0a55ae796524f1f6
strat mil's <pmilambo47@gmail.com>
2024-06-09 14:20:04 +0100

    decoder and coder with impair message

--- index.php
+++ index.php
@@ -47,7 +47,7 @@ if(isset($_POST) and !empty($_POST)){
             $longueur = strlen(htmlentities($_POST['nom']));
             
             if( $longueur % 2 == 0 ){
-                $pair=ture;
+                $pair=true;
                 $text =strtoupper($_POST['nom']);
                 $tableauNom=preg_split('//', $text, -1, PREG_SPLIT_NO_EMPTY);
            
@@ -153,21 +153,21 @@ if(isset($_POST) and !empty($_POST)){
                     $Running = $longueur - 1;
                     for($i=0; $i <= $Running; $i=$i+2){
                         
-                    $NumberPlace1 = $m1 * $tableauNom[$i] +  $m2 * $tableauNom[$i+1];
-                    $NumberPlace2 = $m3 * $tableauNom[$i] +  $m4 * $tableauNom[$i+1];
-                        $TableOfCriptName[]=$NumberPlace1;
-                        $TableOfCriptName[]=$NumberPlace2;
+                        $NumberPlace1 = $m1 * $tableauNom[$i] +  $m2 * $tableauNom[$i+1];
+                        $NumberPlace2 = $m3 * $tableauNom[$i] +  $m4 * $tableauNom[$i+1];
+                            $TableOfCriptName[]=$NumberPlace1;
+                            $TableOfCriptName[]=$NumberPlace2;
                         
-                }
+                    }
                 
-            }
+                }
             }
         }
 
     }
 }
 
-if(isset($longueur) and is_array($TableOfCriptName) and $pair=true){ 
+if(isset($longueur) and is_array($TableOfCriptName) and $pair==true){ 
   
     for($i=0; $i <= $longueur -1 ; $i++){              
         foreach($Alphabet as $place => $letter ){                       
@@ -184,9 +184,11 @@ if(isset($longueur) and is_array($TableOfCriptName) and $pair=true){
     $show=true;
     $longueur = $longueur-1;
 }
-if(isset($longueur) and is_array($TableOfCriptName) and $pair=false){ 
-  
-    for($i=0; $i <= $longueur -2 ; $i++){   
+if(isset($longueur) and is_array($TableOfCriptName) and $pair==false){ 
+
+    ($_POST['choix']=="coder") ? $longueur = $longueur -2 :  $longueur = $longueur -1;
+    
+    for($i=0; $i <= $longueur; $i++){   
                    
         foreach($Alphabet as $place => $letter ){   
                             
@@ -204,7 +206,7 @@ if(isset($longueur) and is_array($TableOfCriptName) and $pair=false){
         $verify=false;
     }
     $show=true;
-    $longueur = $longueur -2;
+   
 }
 
 ?>
@@ -219,14 +221,18 @@ if(isset($longueur) and is_array($TableOfCriptName) and $pair=false){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-<h2 class="bg-info w-20 text-center">Bienvenu sur EncodeSpace 🎃</h2> 
+<h2 class="bg-info w-20 text-center text-white py-2">Bienvenu sur EncodeSpace 🎃</h2> 
+  <!-- Button trigger modal -->
+  <?php if(isset($show) and !$show==true):?>
+    <div class="text-center mb-2">   <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Help system</button></div>
+  <?php endif?>
 <?php if(isset($show) and $show==true):?>
  <div class="text-center" id="message">   
-        <div class="btn btn-secondary mb-2">
+        <div class="btn btn-secondary mb-3">
             <span class="badge rounded-pill bg-success"><?=$message1?>: </span>
             <h6>
             <?php for($i=0; $i <= $longueur ; $i++):?>
-                <?=$TableOfCriptName[$i]." "?>
+               <span class="badge bg-primary"> <?=$TableOfCriptName[$i]." "?></span>
             <?php endfor ?>
             </h6> 
             
@@ -252,7 +258,7 @@ if(isset($longueur) and is_array($TableOfCriptName) and $pair=false){
        <span class="badge bg-warning text-dark mb-1">Taper Du text</span>
 
        <div class="mb-3">
-        <input type="text" name="nom"  placeholder="entrer le mot" class="form-control" required>
+        <input type="text" name="nom"  placeholder="Entrez le message" class="form-control" required>
        </div>
       
        <span class="badge bg-warning text-dark mb-1">selectionner une action</span>
@@ -269,6 +275,62 @@ if(isset($longueur) and is_array($TableOfCriptName) and $pair=false){
       
    </form>
   </div>
+  <?php if(isset($show) and $show==true):?>
+    <hr>
+    <div class="text-center mt-2">   <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Help system</button></div>
+  <?php endif?>
+
+
+
+<!-- Modal -->
+
+<div class="modal fade" id="exampleModalToggle" aria-hidden="true" aria-labelledby="exampleModalToggleLabel" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalToggleLabel">Mode d'utilisation</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+      <h5> Bienvenu sur EncodeSpace,
+            </h5>
+            <p class="text-secondary">
+                Encodespace vous permet de coder et décoder un message avec une matrice carrée que vous allez définir comme mot de passe du codage ou décodage.
+            </p>   
+            <p class="text-secondary">
+                <span class="text-success">Coder un message 🔐</span>: il suffit de remplir la matrice et ainsi entrer un messsage dans la zone appropriée pour le message . En suite selectionner Encoder <br>
+                <span class="text-danger">Décoder un message 🔓</span>: il suffit de remplir la matrice avec laquelle le message a été codé et ainsi entrez l'encodage numérique dans la zone appropriée pour le message. En suite selectionner Décoder
+            </p> .
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button class="btn btn-primary" data-bs-target="#exampleModalToggle2" data-bs-toggle="modal">Voir Les contraintes</button>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="modal fade" id="exampleModalToggle2" aria-hidden="true" aria-labelledby="exampleModalToggleLabel2" tabindex="-1">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalToggleLabel2">Contraintes d'utilisations</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p class="btn btn-info text-white">
+            <span class="btn btn-danger ">⛔ En effet, l'encodage et le décodage des caractères spécial ne pas pris en charge</span>
+            Example: <span class="badge bg-warning"> $/%@*é"'(-èçà")+&é_è('')'*=àé'-| ^@~#{{}} ...</span>
+            Veuillez respecter cette conterainte pour une meilleur utilisation de l'outil.
+        </p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button class="btn btn-primary" data-bs-target="#exampleModalToggle" data-bs-toggle="modal">Back Mode d'utilisation</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <style>
     #formulaire{
         margin-left: 32%;
@@ -290,7 +352,7 @@ if(isset($longueur) and is_array($TableOfCriptName) and $pair=false){
         border: 2px black solid;
     }
 </style>
-
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <footer>
 <i class="bg-info w-20 text-center">@all right <i>stg mil's</i> 🎃</i> 
 </footer>
